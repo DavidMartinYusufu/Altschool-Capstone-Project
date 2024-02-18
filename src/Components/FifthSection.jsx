e54de407f8aabe18b6031519273a9d52e8246eb9
David Martin Yusufu <davidmartinyusufu@gmail.com>
2024-02-18 22:21:40 +0100

    playing around

--- src/Components/FifthSection.jsx
+++ src/Components/FifthSection.jsx
@@ -5,7 +5,7 @@ function FifthSection() {
             <section className=" bg-white border-[1px] border-blue-500 w-4/12 m-auto h-[200px]">
                 <form action="" className="flex flex-col">
                     <input type="text"  className="border-[1px] border-blue-500 w-8/12 m-auto rounded"/>
-                    <button className="bg-blue-500 text-white py-[8px] px-[20px] rounded-[100px] w-8/12 m-auto  ">Shorten URL</button>
+                    <button className="bg-blue-500 text-white py-[8px] px-[20px] rounded-[100px] w-8/12 m-auto  ">Shortens URL</button>
                 </form>
             </section>
         </section>
